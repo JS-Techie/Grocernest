2bccb387841fbff24ac96e2407e29728ef2cec9d
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2022-06-16 17:14:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ const cors = require("cors");
 const dbConfig = require("./config/db.config.js");
 const Sequelize = require("sequelize");
 
-require("dotenv").config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const port = process.env.PORT || 4000
-
-
-// database con.
-
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
@@ -34,6 +21,23 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
+const TCustomerModel = sequelize.import("./models/t_customer");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 4000
+
+
+// database con.
+
+
+
 // module.exports = db;
 
 app.listen(port, () => {
@@ -45,8 +49,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/registration', (req, res) => {
-    res.send('Hello World!')
+app.get('/registration', async function (req, res) {
+    const test = await TCustomerModel.findAll();
+
+    console.log(test);
 })
 
 
