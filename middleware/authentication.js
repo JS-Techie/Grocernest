56352658ecad5055cf3bee494dd6469ad76ceeeb
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-06-30 20:13:51 +0530

    Error handling in middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -5,7 +5,11 @@ const authenticate = async (req, res, next) => {
   //Check header for access token
   const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer')) {
-    throw new Error('Access token not found')
+    return res.status(404).send({
+      success : false,
+      data : null,
+      message : "Access token not found!!!!"
+    })
   }
   //Get the token from the header
   const token = authHeader.split(' ')[1]
@@ -17,8 +21,11 @@ const authenticate = async (req, res, next) => {
      next();
 
   } catch (error) {
-    console.log(error.message)
-    throw new Error('Access token Invalid/Expired')
+    return res.status(400).send({
+      success : false,
+      data : error.message,
+      message : "Access token invalid/expired"
+    })
   }
 }
 
