e9d460d571e16421d2132b9343395f84f7ec28b5
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2022-07-04 18:22:04 +0530

    Referral routes

--- controllers/referralController.js
+++ controllers/referralController.js
@@ -1,9 +1,53 @@
-const getCode = async (req, res, next) => {
+const db = require("../models");
+const Customer = db.CustomerModel;
 
+const getCode = async (req, res, next) => {
+    //Get current user from JWT
+    const currentUser = req.cust_no;
+
+    console.log("get code", currentUser);
+    Customer.findOne({
+        attributes: ['referral_code'],
+        where: {
+            cust_no: currentUser,
+        }
+    }).then((resData) => {
+        return res.status(201).json({
+            success: true,
+            data: resData,
+            message: "Successfully fetched referral code",
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            success: false,
+            data: error.message,
+            message: "Error while fetching referral code",
+        });
+    })
 }
 
 const getMembers = async (req, res, next) => {
+    //Get current user from JWT
+    const currentUser = req.cust_no;
 
+    Customer.findAll({
+        attributes: ["cust_no", "cust_name", "email", "contact_no"],
+        where: {
+            referred_by: currentUser,
+        }
+    }).then((resData) => {
+        return res.status(201).json({
+            success: true,
+            data: resData,
+            message: "Successfully fetched referred members",
+        });
+    }).catch((err) => {
+        return res.status(400).json({
+            success: false,
+            data: error.message,
+            message: "Error while fetching referred members",
+        });
+    })
 }
 
 module.exports = {
