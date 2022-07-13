a010cbd0b3fde78a90829bbf4429189395de445c
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-07-13 16:28:14 +0530

    orders order

--- routes/ordersRoutes.js
+++ routes/ordersRoutes.js
@@ -1,30 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-
-
-const authenticate = require("../middleware/authentication")
-
-
+const authenticate = require("../middleware/authentication");
 
 const {
-    getAllOrders,
-    getOrderByOrderId,
-    cancelOrder,
-    returnOrder,
-    trackOrder
-
-} = require("../controllers/ordersController")
+  getAllOrders,
+  getOrderByOrderId,
+  cancelOrder,
+  returnOrder,
+  trackOrder,
+} = require("../controllers/ordersController");
 
 const { getOrderStatus } = require("../controllers/adminOrdersController");
 
 //Add authenticate middleware to all controllers
 
-router.route('/all').get(authenticate, getAllOrders)
-router.route('/:orderId').get(authenticate, getOrderByOrderId)
-router.route('/cancel/:orderId').post(authenticate, cancelOrder)
-router.route('/return/:orderId').post(authenticate, returnOrder)
-router.route('/:orderId/tracking').get(authenticate, trackOrder)
-
+router.route("/all").get(authenticate, getAllOrders);
+router.route("/:orderId").get(authenticate, getOrderByOrderId);
+router.route("/cancel/:orderId").post(authenticate, cancelOrder);
+router.route("/return/:orderId").post(authenticate, returnOrder);
+router.route("/:orderId/tracking").get(authenticate, trackOrder);
 
 module.exports = router;
