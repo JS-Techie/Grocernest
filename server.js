f5b7a902179ece68f69700b16de468c6ad3128b5
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-07-26 13:39:17 +0530

    Profile image

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const fileUpload = require("express-fileupload");
 const xss = require("xss-clean");
 app.use(cors());
 app.use(express.json({limit : "50mb"}));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use(fileUpload());
 app.use(xss());
 
