ccb0e32cccd7489e7117fcfbeb1239febfc4a025
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-08-11 15:29:22 +0530

    Batch logic complete redefine

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -393,7 +393,7 @@ const getItemById = async (req, res, next) => {
       ,t_item.image ,t_item.description ,t_item.available_for_ecomm ,t_batch.batch_no ,
       t_batch.location_id ,t_batch.MRP ,t_batch.discount ,t_batch.cost_price ,t_batch.mfg_date ,t_batch.sale_price ,
       t_batch.expiry_date,
-      t_batch.created_at,t_lkp_color.color_name,t_batch.quantity, t_lkp_brand.brand_name,t_batch.mark_selected
+      t_batch.created_at,t_lkp_color.color_name,t_batch.quantity, t_lkp_brand.brand_name,t_batch.mark_selected,t_batch.id as "batch_id"
       from ((((((ecomm.t_item
             inner join t_batch on t_batch.item_id = t_item.id )
             inner join t_lkp_color on t_lkp_color.id = t_item.color_id)
