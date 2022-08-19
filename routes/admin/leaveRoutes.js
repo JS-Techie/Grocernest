c535e1649ea199d63c1d2f0fb079a9e793267f0c
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-08-19 16:51:17 +0530

    Change in routes for leave

--- routes/admin/leaveRoutes.js
+++ routes/admin/leaveRoutes.js
@@ -18,10 +18,10 @@ const {
 router.route("/view/all").get(authenticateAdmin, getAllLeaves);
 router.route("/view/:id").get(authenticateAdmin, getLeaveById);
 router.route("/view/:status").get(authenticateAdmin, getLeaveByStatus);
-router.route("/view/:userId").get(authenticateAdmin,getLeaveByUserId)
+router.route("/view/:userId").get(authenticateAdmin, getLeaveByUserId);
 router.route("/approve/:id").patch(authenticateAdmin, approveLeave);
-router.route("cancel/:id").patch(authenticateAdmin, rejectLeave);
-router.route("/create").post(authenticateAdmin,createLeave);
+router.route("/cancel/:id").patch(authenticateAdmin, rejectLeave);
+router.route("/create").post(authenticateAdmin, createLeave);
 router.route("/edit/:id").patch(authenticateAdmin, editLeave);
 router.route("/delete/:id").delete(authenticateAdmin, deleteLeave);
 
