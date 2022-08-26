4a55aca7a5f6f788cb97059cb354f5d0f25398a1
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-08-26 11:17:11 +0530

    Whatsapp message enhancement

--- config/db.actual.config.js
+++ config/db.actual.config.js
@@ -4,6 +4,11 @@
 
 //We will put all the details in .env file and not keep it in config, this is just for development purpose
 
+
+//Old database  -------> ecomm
+//New database -------> grocernest_pre_prod
+
+
 module.exports = {
     HOST: "152.67.1.126",
     USER: "ecomm",
