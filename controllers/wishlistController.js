4a801f44f14b88c28a09a8286e590bff659dee55
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-08-28 17:11:37 +0530

    Wishlist fix

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -18,18 +18,30 @@ const getWishlist = async (req, res, next) => {
   const currentUser = req.cust_no;
 
   try {
-    const [wishlist, metadata] =
-      await sequelize.query(`select t_wishlist_items.item_id,t_item.name, t_item.image, t_item.category_id, t_item.sub_category_id,t_item.description, t_item.UOM,
-      t_batch.MRP,t_batch.sale_price, t_batch.discount,t_lkp_color.color_name, t_lkp_brand.brand_name, t_lkp_category.group_name,
-      t_lkp_sub_category.sub_cat_name, t_batch.mfg_date
-      from ((((((t_wishlist_items
-        inner join t_item on t_item.id = t_wishlist_items.item_id)
-      inner join t_lkp_category on t_lkp_category.id = t_item.category_id)
-      INNER join t_lkp_sub_category on t_lkp_sub_category.id = t_item.sub_category_id)
-      inner join t_batch on t_batch.item_id = t_wishlist_items.item_id )
-      inner join t_lkp_color on t_lkp_color.id = t_item.color_id )
-      inner join t_lkp_brand on t_lkp_brand.id = t_item.brand_id )
-      where t_wishlist_items.cust_no = "${currentUser}"`);
+    const [wishlist, metadata] = await sequelize.query(` select 
+      t_wishlist_items.item_id, 
+      t_item.name, 
+      t_item.image, 
+      t_item.category_id, 
+      t_item.sub_category_id, 
+      t_item.description, 
+      t_item.UOM, 
+      t_lkp_category.group_name, 
+      t_lkp_sub_category.sub_cat_name 
+    from 
+      (
+        (
+          (
+            grocernest_pre_prod.t_wishlist_items 
+            inner join t_item on t_item.id = t_wishlist_items.item_id
+          ) 
+          inner join t_lkp_category on t_lkp_category.id = t_item.category_id
+        ) 
+        INNER join t_lkp_sub_category on t_lkp_sub_category.id = t_item.sub_category_id
+      ) 
+    where 
+      t_wishlist_items.cust_no = "${currentUser}"
+    `);
 
     if (wishlist.length === 0) {
       return res.status(404).send({
@@ -38,6 +50,8 @@ const getWishlist = async (req, res, next) => {
         message: "No wishlist found for current user",
       });
     }
+
+    console.log(wishlist);
 
     const promises = wishlist.map(async (current) => {
       const oldestBatch = await Batch.findOne({
@@ -58,7 +72,10 @@ const getWishlist = async (req, res, next) => {
         const offer = await Offers.findOne({
           where: {
             is_active: 1,
-            [Op.or]: [{ item_id_1: current.item_id }, { item_id: current.item_id }],
+            [Op.or]: [
+              { item_id_1: current.item_id },
+              { item_id: current.item_id },
+            ],
           },
         });
 
