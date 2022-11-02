59119cf4778bc549d25820c6b048cc791c89c81f
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-11-02 13:44:23 +0530

    Edit Images and uploaded images

--- routes/admin/itemRoutes.js
+++ routes/admin/itemRoutes.js
@@ -4,7 +4,12 @@ const authenticateAdmin = require("../../middleware/authenticateAdmin");
 
 const {
   uploadMultipleImages,
+  editUploadedImages,
+  deleteImage,
 } = require("../../controllers/admin/itemController");
+
 router.route("/upload/images").post(authenticateAdmin, uploadMultipleImages);
+router.route("/edit/images").post(authenticateAdmin, editUploadedImages);
+router.route("/delete/images").post(authenticateAdmin, deleteImage);
 
 module.exports = router;
