d6ffd0d8c656c8981f78d60230aca26bef915ad8
mehul-tuteck <mehul.chattopadhyay@tuteck.com>
2022-12-05 16:40:30 +0530

    Orders by ordered date and delivery date

--- routes/admin/dailySalesRoutes.js
+++ routes/admin/dailySalesRoutes.js
@@ -3,10 +3,11 @@ const router = require("express").Router();
 const authenticateAdmin = require("../../middleware/authenticateAdmin");
 
 const {
-    getOrdersByDate,
-
+  getOrdersByDate,
+  getOrdersByDeliveryBoy,
 } = require("../../controllers/admin/dailySalesController");
 
 router.route("/view").post(getOrdersByDate);
+router.route("/view/delivery").post(getOrdersByDeliveryBoy);
 
 module.exports = router;
