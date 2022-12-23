c3c05f4cc2190ad3d109971e6209ca36bfde3c77
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2022-12-23 16:56:39 +0530

    Update couponToCustomerController.js

--- controllers/admin/couponToCustomerController.js
+++ controllers/admin/couponToCustomerController.js
@@ -227,7 +227,7 @@ const displayMappedCouponToCustomer = async (req, res, next) => {
       `select tccm.cust_id, tccm.coupon_id, tc.cust_name, tc.contact_no,
             tccm.coupon_name, tccm.assignment_date, tccm.expiry_date, tccm.coupon_used_date
             from t_customer_coupon_mapping tccm 
-            left outer join t_customer tc on tc.cust_no = tccm.cust_id;`
+            left outer join t_customer tc on tc.cust_no = tccm.cust_id order by tccm.assignment_date DESC;`
     );
 
     // console.log(result);
