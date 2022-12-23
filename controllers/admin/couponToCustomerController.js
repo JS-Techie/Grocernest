7094a4de87992c6d91b1a4d1a4a5a2f7c5821cf1
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2022-12-23 15:36:34 +0530

    Update couponToCustomerController.js

--- controllers/admin/couponToCustomerController.js
+++ controllers/admin/couponToCustomerController.js
@@ -7,340 +7,347 @@ const Customer = db.CustomerModel;
 const CustomerToCouponMapping = db.CustomerToCouponMappingModel;
 
 const createCouponToCustomer = async (req, res, next) => {
-    const {
-        coupon_name,
-        coupon_desc,
-        amount_of_discount,
-        duration,
-    } = req.body;
+  const { coupon_name, coupon_desc, amount_of_discount, duration } = req.body;
 
-    const { user_id } = req;
+  const { user_id } = req;
 
-    try {
-
-        if (coupon_name == "" || amount_of_discount == "" || amount_of_discount <= 0 || duration == "" || duration <= 0) {
-            return res.status(400).send({
-                success: false,
-                data: [],
-                message: "Please enter all mandatory field and put valid data",
-            });
-        }
-
-        if (amount_of_discount === 0) {
-            return res.status(400).send({
-                success: false,
-                data: [],
-                message: "Please enter an amount, it cannot be zero",
-            });
-        }
-
-
-
-        // check that coupon code is available already or not
-        const couponAvailable = await CouponToCustomer.findAll({
-            where: {
-                coupon_name
-            }
-        });
-
-        if (couponAvailable.length > 0) {
-            return res.status(400).send({
-                success: false,
-                data: [],
-                message: "Coupon code already exists! try with a different name.",
-            });
-        }
-
-        // coupon code is not already taken, so proceed..
-        let new_id = uniq()
-        const newCoupon = await CouponToCustomer.create({
-            id: new_id,
-            coupon_name,
-            coupon_desc,
-            amount_of_discount,
-            duration,
-            created_by: user_id,
-        });
-
-        return res.status(201).send({
-            success: true,
-            data: newCoupon,
-            message: "New coupon to customer successfully created",
-        });
-    } catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong, please check data field",
-        });
+  try {
+    if (
+      coupon_name == "" ||
+      amount_of_discount == "" ||
+      amount_of_discount <= 0 ||
+      duration == "" ||
+      duration <= 0
+    ) {
+      return res.status(400).send({
+        success: false,
+        data: [],
+        message: "Please enter all mandatory field and put valid data",
+      });
     }
+
+    if (amount_of_discount === 0) {
+      return res.status(400).send({
+        success: false,
+        data: [],
+        message: "Please enter an amount, it cannot be zero",
+      });
+    }
+
+    // check that coupon code is available already or not
+    const couponAvailable = await CouponToCustomer.findAll({
+      where: {
+        coupon_name,
+      },
+    });
+
+    if (couponAvailable.length > 0) {
+      return res.status(400).send({
+        success: false,
+        data: [],
+        message: "Coupon code already exists! try with a different name.",
+      });
+    }
+
+    // coupon code is not already taken, so proceed..
+    let new_id = uniq();
+    const newCoupon = await CouponToCustomer.create({
+      id: new_id,
+      coupon_name,
+      coupon_desc,
+      amount_of_discount,
+      duration,
+      created_by: user_id,
+    });
+
+    return res.status(201).send({
+      success: true,
+      data: newCoupon,
+      message: "New coupon to customer successfully created",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong, please check data field",
+    });
+  }
 };
 
 const displayCouponToCustomer = async (req, res, next) => {
+  try {
+    const allCoupons = await CouponToCustomer.findAll();
 
-    try {
-        const allCoupons = await CouponToCustomer.findAll();
-
-        return res.status(200).send({
-            success: true,
-            data: allCoupons,
-            message: "All coupon to customer fetched successfully",
-        });
-    }
-    catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong.",
-        });
-    }
-}
+    return res.status(200).send({
+      success: true,
+      data: allCoupons,
+      message: "All coupon to customer fetched successfully",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong.",
+    });
+  }
+};
 
 const updateCouponToCustomer = async (req, res, next) => {
+  const { id, coupon_desc, amount_of_discount, duration } = req.body;
+  const { user_id } = req;
 
-    const { id, coupon_desc, amount_of_discount, duration } = req.body;
-    const { user_id } = req;
+  if (
+    amount_of_discount <= 0 ||
+    amount_of_discount == "" ||
+    duration == "" ||
+    duration <= 0
+  ) {
+    return res.status(400).send({
+      success: false,
+      data: [],
+      message: "Please enter all mandatory field and put valid data",
+    });
+  }
 
-    if (amount_of_discount <= 0 || amount_of_discount == "" || duration == "" || duration <= 0) {
-        return res.status(400).send({
-            success: false,
-            data: [],
-            message: "Please enter all mandatory field and put valid data",
-        });
-    }
-
-    try {
-        const updatedCoupon = CouponToCustomer.update(
-            {
-                coupon_desc,
-                amount_of_discount,
-                duration,
-                updated_at: new Date().getTime(),
-                updated_by: user_id
-            },
-            { where: { id } }
-        )
-        return res.status(200).send({
-            success: true,
-            data: updatedCoupon,
-            message: "The coupon to customer edited successfully",
-        });
-    }
-    catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong.",
-        });
-    }
-}
+  try {
+    const updatedCoupon = CouponToCustomer.update(
+      {
+        coupon_desc,
+        amount_of_discount,
+        duration,
+        updated_at: new Date().getTime(),
+        updated_by: user_id,
+      },
+      { where: { id } }
+    );
+    return res.status(200).send({
+      success: true,
+      data: updatedCoupon,
+      message: "The coupon to customer edited successfully",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong.",
+    });
+  }
+};
 
 const deleteCouponToCustomer = async (req, res, next) => {
-    const { id } = req.body;
+  const { id } = req.body;
 
-    try {
-        const deleted = await CouponToCustomer.destroy({
-            where: { id },
-        });
+  try {
+    const deleted = await CouponToCustomer.destroy({
+      where: { id },
+    });
 
-        return res.status(200).send({
-            success: true,
-            data: deleted,
-            message: "The coupon to customer deleted successfully",
-        });
-    }
-    catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong.",
-        });
-    }
-}
-
-
+    return res.status(200).send({
+      success: true,
+      data: deleted,
+      message: "The coupon to customer deleted successfully",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong.",
+    });
+  }
+};
 
 const mapCouponToCustomer = async (req, res, next) => {
-    const { cust_no, coupon_id, coupon_name } = req.body;
-    const { user_id } = req;
+  const { cust_no, coupon_id, coupon_name } = req.body;
+  const { user_id } = req;
 
-    try {
+  try {
+    // check coupon exist or not
+    let couponExist = await CouponToCustomer.findOne({
+      where: {
+        id: coupon_id,
+        coupon_name: coupon_name,
+      },
+    });
 
-        // check coupon exist or not
-        let couponExist = await CouponToCustomer.findOne({
-            where: {
-                id: coupon_id,
-                coupon_name: coupon_name
-            }
-        })
-
-        if (couponExist.length < 1) {
-            return res.status(400).send({
-                success: false,
-                data: "",
-                message: "Coupon Name/Coupon ID does not exist.",
-            });
-        }
-
-        // check customer exist or not
-        let customerExist = await Customer.findAll({
-            where: {
-                cust_no
-            }
-        })
-
-        if (customerExist.length < 1) {
-            return res.status(400).send({
-                success: false,
-                data: "",
-                message: "Customer/Customer ID does not exist.",
-            });
-        }
-
-        var expiry_timestamp = new Date();
-        expiry_timestamp.setDate(expiry_timestamp.getDate() + parseInt(couponExist.duration));
-
-        // everything is valid, now create the map
-        const mapCustomerToCouponData = CustomerToCouponMapping.create({
-            cust_id: cust_no,
-            coupon_id,
-            coupon_name,
-            assignment_date: new Date().getTime(),
-            expiry_date: expiry_timestamp,
-            created_by: user_id
-        })
-
-        return res.status(200).send({
-            success: true,
-            data: mapCustomerToCouponData,   
-            message: "User mapped to the coupon successfully!",
-        });
+    if (couponExist.length < 1) {
+      return res.status(400).send({
+        success: false,
+        data: "",
+        message: "Coupon Name/Coupon ID does not exist.",
+      });
     }
-    catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong while mapping user to the coupon.",
-        });
+
+    // check customer exist or not
+    let customerExist = await Customer.findAll({
+      where: {
+        cust_no,
+      },
+    });
+
+    if (customerExist.length < 1) {
+      return res.status(400).send({
+        success: false,
+        data: "",
+        message: "Customer/Customer ID does not exist.",
+      });
     }
-}
+
+    var expiry_timestamp = new Date();
+    expiry_timestamp.setDate(
+      expiry_timestamp.getDate() + parseInt(couponExist.duration)
+    );
+
+    // everything is valid, now create the map
+    const mapCustomerToCouponData = CustomerToCouponMapping.create({
+      cust_id: cust_no,
+      coupon_id,
+      coupon_name,
+      assignment_date: new Date().getTime(),
+      expiry_date: expiry_timestamp,
+      created_by: user_id,
+    });
+
+    return res.status(200).send({
+      success: true,
+      data: mapCustomerToCouponData,
+      message: "User mapped to the coupon successfully!",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong while mapping user to the coupon.",
+    });
+  }
+};
 
 const applyCoupon = async (req, res, next) => {
-    const { cust_no, coupon_name } = req.body;
+  const { cust_no, coupon_name } = req.body;
 
-    try {
-        let customer_coupon_mapping = {};
-        // check coupon exist or not
-        let couponExist = await CouponToCustomer.findAll({
-            where: {
-                coupon_name: coupon_name
-            }
-        })
+  try {
+    let customer_coupon_mapping = {};
+    // check coupon exist or not
+    let couponExist = await CouponToCustomer.findAll({
+      where: {
+        coupon_name: coupon_name,
+      },
+    });
 
-        if (couponExist.length < 1) {
-            return res.status(400).send({
-                success: false,
-                data: "",
-                message: "Coupon does not exist.",
-            });
-        }
-
-        // check coupon belongs to customer or not
-        let mappingExist = await CustomerToCouponMapping.findAll({
-            where: {
-                cust_id: cust_no,
-                coupon_name
-            },
-            order: [["assignment_date", "ASC"]]
-        })
-
-        console.log(mappingExist.length)
-
-        if (mappingExist.length < 1) {
-            return res.status(400).send({
-                success: false,
-                data: "",
-                message: "Customer don't have this coupon in his account",
-            });
-        }
-
-        // if same coupon exist to same customer more than once, choose the older coupon first
-        if (mappingExist.length > 1) {
-            customer_coupon_mapping = mappingExist[0].dataValues;
-        }
-
-        // check for expired coupons
-
-        // fetch coupon from coupon table
-        // console.log(my_coupon);
-
-        return res.status(200).send({
-            success: true,
-            data: customer_coupon_mapping,
-            message: "Coupon Applied!",
-        });
-    }
-    catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong while mapping user to the coupon.",
-        });
+    if (couponExist.length < 1) {
+      return res.status(400).send({
+        success: false,
+        data: "",
+        message: "Coupon does not exist.",
+      });
     }
 
-}
+    // check coupon belongs to customer or not
+    let mappingExist = await CustomerToCouponMapping.findAll({
+      where: {
+        cust_id: cust_no,
+        coupon_name,
+      },
+      order: [["assignment_date", "ASC"]],
+    });
 
+    // console.log(mappingExist.length);
+
+    if (mappingExist.length < 1) {
+      return res.status(400).send({
+        success: false,
+        data: "",
+        message: "Customer don't have this coupon in his account",
+      });
+    }
+
+    // if same coupon exist to same customer more than once, choose the older coupon first
+    if (mappingExist.length > 1) {
+      customer_coupon_mapping = mappingExist[0].dataValues;
+    }
+
+    // check for expired coupons
+
+    // fetch coupon from coupon table
+    // console.log(my_coupon);
+
+    return res.status(200).send({
+      success: true,
+      data: customer_coupon_mapping,
+      message: "Coupon Applied!",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong while mapping user to the coupon.",
+    });
+  }
+};
 
 const displayMappedCouponToCustomer = async (req, res, next) => {
-
-    try {
-        const [allCustomerToCouponMapping, metadata] = await sequelize.query(
-          `select tccm.cust_id, tccm.coupon_id, tc.cust_name, tc.contact_no,
+  try {
+    const [allCustomerToCouponMapping, metadata] = await sequelize.query(
+      `select tccm.cust_id, tccm.coupon_id, tc.cust_name, tc.contact_no,
             tccm.coupon_name, tccm.assignment_date, tccm.expiry_date, tccm.coupon_used_date
             from t_customer_coupon_mapping tccm 
             left outer join t_customer tc on tc.cust_no = tccm.cust_id;`
-        );
+    );
 
-        // console.log(result);
+    // console.log(result);
 
-        return res.status(200).send({
-            success: true,
-            data: allCustomerToCouponMapping,
-            message: "All coupon to customer mapped data fetched successfully",
-        });
-    }
-    catch (error) {
-        return res.status(400).send({
-            success: false,
-            data: error.message,
-            message: "Something went wrong.",
-        });
-    }
-}
+    return res.status(200).send({
+      success: true,
+      data: allCustomerToCouponMapping,
+      message: "All coupon to customer mapped data fetched successfully",
+    });
+  } catch (error) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong.",
+    });
+  }
+};
 
 const applicableCouponForACustomer = async (req, res, next) => {
-    const { cust_no } = req.body;
-    
-    try {
-        const allApplicableCouponsForThisCustomer = await sequelize.query(
-            ``
-        );
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+  const { cust_no } = req.body;
 
+  try {
+    const [allApplicableCouponsForThisCustomer, metadata] = await sequelize.query(
+      `select tccm.coupon_id, tccm.coupon_name, tctc.amount_of_discount, 
+            tccm.assignment_date ,tccm.expiry_date
+            from t_customer_coupon_mapping tccm 
+            inner join t_coupon_to_customer tctc 
+            where tccm.cust_id ="` +
+        cust_no +
+        `" 
+            and tctc.coupon_name = tccm.coupon_name 
+            and DATE(tccm.expiry_date) >= CURDATE() ORDER by tccm.expiry_date ASC;
+`
+      );
+
+    return res.status(200).send({
+      success: true,
+      data: allApplicableCouponsForThisCustomer,
+      message: "All coupons for this customer fetched successfully",
+    });
+  } catch (err) {
+    return res.status(400).send({
+      success: false,
+      data: error.message,
+      message: "Something went wrong.",
+    });
+  }
+};
 
 module.exports = {
-    createCouponToCustomer,
-    displayCouponToCustomer,
-    updateCouponToCustomer,
-    deleteCouponToCustomer,
+  createCouponToCustomer,
+  displayCouponToCustomer,
+  updateCouponToCustomer,
+  deleteCouponToCustomer,
 
-
-    mapCouponToCustomer,
-    displayMappedCouponToCustomer,
-    applyCoupon, //not done yet,
-    applicableCouponForACustomer
+  mapCouponToCustomer,
+  displayMappedCouponToCustomer,
+  applyCoupon, //not done yet,
+  applicableCouponForACustomer,
 };
