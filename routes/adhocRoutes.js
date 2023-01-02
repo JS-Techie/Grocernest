d6ec360047c23d2f6e27394858d2982c77af993f
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-01-02 11:40:04 +0530

    wallet creation part removed

--- routes/adhocRoutes.js
+++ routes/adhocRoutes.js
@@ -2,12 +2,10 @@ const router = require("express").Router();
 
 const {
   addWalletBalance,
-  checkBatchNo,
-  migrateCustomers,
+  checkBatchNo
 } = require("../controllers/adhocController");
 
 router.route("/itemsp/wallet/add").post(addWalletBalance);
 router.route("/duplicate/batch").post(checkBatchNo);
-router.route("/create/wallet").post(migrateCustomers);
 
 module.exports = router;
