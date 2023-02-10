0376704941485dcbe426989460fc26d4fd444ddf
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-02-10 18:32:59 +0530

    Update checkoutRoutes.js

--- routes/checkoutRoutes.js
+++ routes/checkoutRoutes.js
@@ -1,13 +1,12 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-const authenticate = require("../middleware/authentication")
+const authenticate = require("../middleware/authentication");
 
-const{
-    checkoutFromCart,
-    buyNow
-} = require("../controllers/checkoutController")
-
+const {
+  checkoutFromCart,
+  buyNow,
+} = require("../controllers/checkoutController");
 
 router.route("/cart").post(authenticate, checkoutFromCart);
 router.route("/item").post(authenticate, buyNow);
