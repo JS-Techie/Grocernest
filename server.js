cf8c2005749e3391d67ff948a60283bc32204465
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-02-15 13:52:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.use("/vendor", vendorRouter);
 
 //Start server and connect to DB
 const db = require("./services/dbSetupService.js");
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 const start = async () => {
   try {
     app.listen(PORT, () => {
