2355cea81b097867fe9e5b25258a3e264d6356b6
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-02-21 17:42:28 +0530

    db config change

--- config/db.config.js
+++ config/db.config.js
@@ -19,9 +19,9 @@ require("dotenv").config();
 // };
 
 module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB,
 
   dialect: "mariadb",
