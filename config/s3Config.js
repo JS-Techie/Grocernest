e8b059e153126aabd1dc998754be8ef261c70c76
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-02-21 11:40:47 +0530

    Update s3Config.js

--- config/s3Config.js
+++ config/s3Config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    region: process.env.AWS_BUCKET_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
-  }
+  region: "ap-south-1",
+  accessKeyId: "AKIAZJC7TOCOPHLXCZ7Y",
+  secretAccessKey: "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",
+};
+
+// module.exports = {
+//   region: process.env.AWS_BUCKET_REGION,
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_KEY,
+// };
