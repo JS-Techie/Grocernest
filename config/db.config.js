ab10cfe52dcb0e6dd650428fcc9d1c11c715c746
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-02-21 16:40:51 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,27 +1,8 @@
-// module.exports = {
-//   HOST: "grocernest-dev.cccojqtgiuzf.ap-south-1.rds.amazonaws.com",
-//   USER: "admin",
-//   PASSWORD: "grocernest2021#",
-//   DB: "grocernest_pre_prod",
-
-//   dialect: "mariadb",
-//   define: {
-//     timestamps: false,
-//   },
-//   timestamps: false,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
-
 module.exports = {
-  HOST: process.env.HOST,
-  USER: process.env.USER,
-  PASSWORD: process.env.PASSWORD,
-  DB: process.env.DB,
+  HOST: "grocernest-dev.cccojqtgiuzf.ap-south-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "grocernest2021",
+  DB: "grocernest_pre_prod",
 
   dialect: "mariadb",
   define: {
@@ -35,3 +16,22 @@ module.exports = {
     idle: 10000,
   },
 };
+
+// module.exports = {
+//   HOST: process.env.HOST,
+//   USER: process.env.USER,
+//   PASSWORD: process.env.PASSWORD,
+//   DB: process.env.DB,
+
+//   dialect: "mariadb",
+//   define: {
+//     timestamps: false,
+//   },
+//   timestamps: false,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
