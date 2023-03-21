af76d62365536c54f7b92352edf2c1be428bd724
tanmoyUNBchandra <tanmoy.chandra@tuteck.com>
2023-03-21 15:22:33 +0530

    added balance in get wallet data api

--- routes/admin/customerRoutes.js
+++ routes/admin/customerRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const authenticateAdmin = require("../../middleware/authenticateAdmin")
+const authenticateAdmin = require("../../middleware/authenticateAdmin");
 
 const {
-    getCustomerList
+  getCustomerList,
 } = require("../../controllers/admin/customerController.js");
 
-router.route('/get/customer/list/:phno').get(authenticateAdmin, getCustomerList);
-router.route('/get/customer/list').get(authenticateAdmin, getCustomerList);
-
-
+router
+  .route("/get/customer/list/:phno")
+  .get(authenticateAdmin, getCustomerList);
+router.route("/get/customer/list").get(getCustomerList);
 
 module.exports = router;
